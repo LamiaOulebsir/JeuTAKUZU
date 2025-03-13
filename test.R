ceffd8d8ec43d14740303b826190d8b0bae6aa03
Lamia Oulebsir <claraouche2002@gmail.com>
2025-03-13 21:17:44 +0100

    Tester si les fonctions c++ marchent

---
+++ test.R
@@ -0,0 +1,9 @@
+library(JeuTAKUZU)
+# Créer une matrice 3x3 avec des 0 et des 1
+mat_test <- matrix(c(0, 1, 0,
+                     1, 0, 1,
+                     1, 0, 0),
+                   nrow = 3, byrow = TRUE)
+
+check_consecutive(mat_test, 3)
+
