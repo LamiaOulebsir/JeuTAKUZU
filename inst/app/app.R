cff557b89727e1d1fc010115decbabe195643e1d
Lamia Oulebsir <claraouche2002@gmail.com>
2025-03-20 17:05:41 +0100

    affichage de la grille

--- inst/app/app.R
+++ inst/app/app.R
@@ -7,7 +7,8 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       numericInput("n", "Taille de la grille", 5, min = 3, max = 10),
-      actionButton("start", "Démarrer le jeu")
+      actionButton("start", "Démarrer le jeu"),
+      actionButton("Check" ,"Check")  # Vérifier si le joueur a gangné ou pas
     ),
 
     mainPanel(
@@ -18,7 +19,26 @@ ui <- fluidPage(
 
 
 server <- function(input, output, session) {
+  btn_values <- reactiveValues() # stocker la matrice de boutons
 
+  # Initialisation de la matrice lorsqu'on appuie sur "Démarrer le jeu"
+  ObserEvent(input$start, {
+    n <- input$n
+    btn_values$matrix <- matric(sample(1,0,""),n*n ,replace =TRUE)
+
+  })
+
+  # Créer et afficher la grille de boutons
+  output$button_grid <- renderUI({
+    n <- input$n
+    div(
+      class = "grid-container",
+      style = paste0("display: grid; grid-template-columns: repeat(", n, ", 50px); grid-gap: 0px;"),
+      lapply(1:(n * n), function(i) {
+        actionButton(inputId = paste0("btn_", i), label = btn_values$matrix[i], style = "width: 50px; height: 50px; margin: 0;")
+      })
+    )
+  })
 
 
 }
